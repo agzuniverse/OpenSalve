8d38d810bd81982c029ddb048f47e039ccf20810
Aswin G <aswinganesh666@gmail.com>
2018-12-12 17:19:34 +0530

    heroku config part 5

--- opensalve/settings.py
+++ opensalve/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = '1'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["0.0.0.0", "localhost"]
+ALLOWED_HOSTS = ["0.0.0.0", "localhost","desolate-lowlands-99133.herokuapp.com"]
 
 
 # Application definition
